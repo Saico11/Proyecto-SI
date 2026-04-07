3b1cb52c1366432ba7c16bf9b2762c9bf2e00164
Marcelo Avendaño <m4fb@kali.m4fb.com>
2026-04-06 23:56:23 -0500

    Laptops y complementos añadidos

--- Sistema_Recomendacion_de_Productos.pl
+++ Sistema_Recomendacion_de_Productos.pl
@@ -40,3 +40,42 @@ almacenamiento(ssd512).
 almacenamiento(ssd1tb).
 almacenamiento(samsung_990_pro_4tb).
 almacenamiento(crucial_t705_2tb).
+
+% =========================
+% LAPTOPS
+% =========================
+
+laptop(gamer, asus_rog_strix_scar_18_2026).
+laptop(gamer, msi_raider_ge78_hx).
+
+laptop(productividad_diseno, apple_macbook_pro_16_m4_max).
+
+laptop(corporativa_trabajo, lenovo_thinkpad_x1_carbon_gen_13).
+
+laptop(workstation, precision_7780_workstation).
+laptop(workstation, hp_zbook_fury_g10).
+
+% =========================
+% OFERTAS Y COMPATIBILIDAD
+% =========================
+
+oferta_descuento(15).
+
+compatibilidad(procesador(intel_core_i9), gpu(nvidia_geforce_rtx_4090)).
+compatibilidad(procesador(amd_ryzen9_9950x3d), gpu(asus_rog_strix_rtx_4080_super)).
+compatibilidad(procesador(intel_core_i7), gpu(rtx4070)).
+
+% =========================
+% PRODUCTOS COMPLEMENTARIOS
+% =========================
+
+complementario(laptop, mouse).
+complementario(laptop, teclado).
+complementario(laptop, parlantes).
+complementario(laptop, audifonos).
+complementario(laptop, mochila).
+
+complementario(pc, monitor).
+complementario(pc, teclado).
+complementario(pc, mouse).
+complementario(pc, parlantes).
